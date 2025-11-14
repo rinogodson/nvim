c850ce37fcf427594d605f21d540f7831e6b9008
Rino Godson <rinogodson@gmail.com>
2025-11-14 06:08:25 +0530

    configured telescopr for better accessibility

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,6 +1,7 @@
 return {
   settings = {
     Lua = {
+      hint = { enable = true },
       diagnostics = { globals = { "vim" } },
       completion = { callSnippet = "Replace" },
     },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,14 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("telescope").setup({
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+        },
+        live_grep = {
+          theme = "ivy",
+        },
+      },
       defaults = {
         layout_config = {
           horizontal = {
